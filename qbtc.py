cd0c60ae65bd69961956ba1d26de2b6f9587376f
Alfred Wu <alfred.nan.wu@gmail.com>
2025-04-04 12:29:47 +0800

    milestone 1

--- qbtc.py
+++ qbtc.py
@@ -9,13 +9,13 @@ start_date = "2020-01-01"
 end_date = datetime.datetime.today().strftime("%Y-%m-%d")
 
 # Download historical Nasdaq-100 Futures data using yfinance
-data = yf.download(ticker, start=start_date, end=end_date)
+Nas_data = yf.download(ticker, start=start_date, end=end_date)
 
 # Retrieve the 10-Year Breakeven Inflation Rate from FRED
 breakeven = web.DataReader('T10YIE', 'fred', start_date, end_date)
 
 # Download historical DS Dollar Index (using "DX-Y.NYB") data using yfinance
-Dx_data = yf.download("DX-Y.NYB", start=start_date, end=end_date)
+usd_data = yf.download("DX-Y.NYB", start=start_date, end=end_date)
 
 # Download historical Bitcoin daily data using yfinance
 btc_data = yf.download('BTC-USD', start=start_date, end=end_date)
@@ -24,8 +24,8 @@ btc_data = yf.download('BTC-USD', start=start_date, end=end_date)
 fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(12, 12), sharex=True)
 
 # First subplot: Nasdaq-100 Futures Close Price
-ax1.plot(data.index, data['Close'], label='Close Price')
-ax1.set_ylabel('Close Price')
+ax1.plot(Nas_data.index, Nas_data['Close'], label='Nasdaq-100 Futures')
+ax1.set_ylabel('Nasdaq-100 Futures')
 ax1.set_title('Nasdaq-100 Futures Close Price (2020 - Today)')
 ax1.legend()
 ax1.grid(True)
@@ -38,7 +38,7 @@ ax2.legend()
 ax2.grid(True)
 
 # Third subplot: DS Dollar Index Data
-ax3.plot(Dx_data.index, Dx_data['Close'], label='Close Price')
+ax3.plot(usd_data.index, usd_data['Close'], label='DS Dollar Index')
 ax3.set_ylabel('DS Dollar Index Close Price')
 ax3.set_title('DS Dollar Index (2020 - Today)')
 ax3.legend()
